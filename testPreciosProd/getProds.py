b78977e3d9b98ffdecfafd8f4a69439e7fcdd4e4
armenmorcecian <31047046+armenmorcecian@users.noreply.github.com>
2018-05-29 02:07:34 +0200

    Update getProds.py

--- testPreciosProd/getProds.py
+++ testPreciosProd/getProds.py
@@ -1,12 +1,36 @@
 import urllib3
-import certifi
 import json
+import requests
+import certifi
+import os
+
+sucn3=0
+sucursal = '10-3-'+str(sucn3)
+offset = 0
 
 
-http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
-r = http.request('GET', 'https://d3e6htiiul5ek9.cloudfront.net/prod/productos?id_sucursal=10-3-251&limit=100&offset=99')
-obj = json.load(r.data)
+#con urllib3
+#http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
+#r = http.request('GET', url)
+#obj = json.load(r.data)
 
+#con requests
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 
+while sucn3 <= 99:
 
+    if not os.path.exists(sucursal):
+        os.makedirs(sucursal)
 
+    while offset <= 1000:
+        url = 'https://d3e6htiiul5ek9.cloudfront.net/prod/productos?id_sucursal=' + sucursal + '&limit=100&offset=' + str(
+            offset)
+        r2 = requests.get(url, headers=headers).json()
+        filename = sucursal+'/'+sucursal+'_'+str(offset)+'.json'
+
+        with open(filename, 'w') as outfile:
+            json.dump(r2, outfile)
+
+        offset = offset+99
+
+    sucn3 = sucn3+1
