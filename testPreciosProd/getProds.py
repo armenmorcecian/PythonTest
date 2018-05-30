95c2c834a0963a4bfb27542bb8bdd8a2146046b5
armenmorcecian <31047046+armenmorcecian@users.noreply.github.com>
2018-05-31 01:44:13 +0200

    Update getProds.py

--- testPreciosProd/getProds.py
+++ testPreciosProd/getProds.py
@@ -1,15 +1,17 @@
-import urllib3
 import json
 import requests
-import certifi
 import os
+from decimal import Decimal
 
 
 tipodato = 'productos'
-elementPos = 0
-tipodatoList = {}
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 sucursales = []
+
+#limites sucursales capital
+#-34.599722, -58.381944
+#-34.691235, -58.354478
+
 
 # creo la carpeta
 if not os.path.exists(tipodato):
@@ -20,12 +22,18 @@ with open('sucursales/sucursales.json', 'r') as fileSucursales:
     jsonSucursales = json.load(fileSucursales)
 
     for elemSucursal in jsonSucursales:
-        sucursales.append(jsonSucursales[elemSucursal]['id'])
+        if (Decimal(jsonSucursales[elemSucursal]['lat']) >= -34.691235) \
+                and (Decimal(jsonSucursales[elemSucursal]['lat']) <= -34.599722) \
+                and (Decimal(jsonSucursales[elemSucursal]['lng']) >= -58.381944) \
+                and (Decimal(jsonSucursales[elemSucursal]['lng']) <= -58.354478):
+            sucursales.append(jsonSucursales[elemSucursal]['id'])
 
 for sucursal in sucursales:
 
     filename = tipodato + '/' + sucursal + '.json'
     offset = 0
+    elementPos = 0
+    tipodatoList = {}
 
     #creo el archivo
     if os.path.exists(filename):
