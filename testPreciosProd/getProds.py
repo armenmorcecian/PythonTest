081330fb0120bf867b12c0236ed0f48872a1b781
armenmorcecian <31047046+armenmorcecian@users.noreply.github.com>
2018-06-01 15:05:53 +0200

    Add files via upload

--- testPreciosProd/getProds.py
+++ testPreciosProd/getProds.py
@@ -3,60 +3,63 @@ import requests
 import os
 from decimal import Decimal
 
-
-tipodato = 'productos'
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-sucursales = []
-
 #limites sucursales capital
-#-34.599722, -58.381944
-#-34.691235, -58.354478
+#-34.68000, -58.56000
+#-34.50000, -58.31000
+
+def importarProductos():
+
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    tipodato = 'productos'
+    sucursales = []
+
+    # creo la carpeta
+    if not os.path.exists(tipodato):
+        os.makedirs(tipodato)
+
+    #traigo sucursales
+    with open('sucursales/sucursales.json', 'r') as fileSucursales:
+        jsonSucursales = json.load(fileSucursales)
+
+        for elemSucursal in jsonSucursales:
+            if (float(jsonSucursales[elemSucursal]['lat']) >= -34.69000) \
+                    and (float(jsonSucursales[elemSucursal]['lat']) <= -34.50000) \
+                    and (float(jsonSucursales[elemSucursal]['lng']) >= -58.56000) \
+                    and (float(jsonSucursales[elemSucursal]['lng']) <= -58.31000):
+                sucursales.append(jsonSucursales[elemSucursal]['id'])
+
+    for sucursal in sucursales:
+
+        filename = tipodato + '/' + sucursal + '.json'
+        offset = 0
+        elementPos = 0
+        tipodatoList = {}
+
+        #creo el archivo
+        if not os.path.exists(filename):
+            #os.remove(filename)
+            urlCant = 'https://d3e6htiiul5ek9.cloudfront.net/prod/productos?id_sucursal=' + sucursal + '&limit=100&offset=' + str(
+                        offset)
+            rTot = requests.get(urlCant, headers=headers).json()
+
+            if isinstance(rTot['total'], int):
+
+                totalElementos = rTot['total']
+
+                while offset <= totalElementos:
+                    url = 'https://d3e6htiiul5ek9.cloudfront.net/prod/productos?id_sucursal=' + sucursal + '&limit=100&offset=' + str(
+                        offset)
+                    r2 = requests.get(url, headers=headers).json()
+
+                    for element in r2['productos']:
+                        tipodatoList[elementPos] = element
+                        elementPos = elementPos + 1
+
+                    offset = offset + 99
+
+            with open(filename, 'w') as outfile:
+                json.dump(tipodatoList, outfile)
+            print(filename)
 
 
-# creo la carpeta
-if not os.path.exists(tipodato):
-    os.makedirs(tipodato)
-
-#traigo sucursales
-with open('sucursales/sucursales.json', 'r') as fileSucursales:
-    jsonSucursales = json.load(fileSucursales)
-
-    for elemSucursal in jsonSucursales:
-        if (Decimal(jsonSucursales[elemSucursal]['lat']) >= -34.691235) \
-                and (Decimal(jsonSucursales[elemSucursal]['lat']) <= -34.599722) \
-                and (Decimal(jsonSucursales[elemSucursal]['lng']) >= -58.381944) \
-                and (Decimal(jsonSucursales[elemSucursal]['lng']) <= -58.354478):
-            sucursales.append(jsonSucursales[elemSucursal]['id'])
-
-for sucursal in sucursales:
-
-    filename = tipodato + '/' + sucursal + '.json'
-    offset = 0
-    elementPos = 0
-    tipodatoList = {}
-
-    #creo el archivo
-    if not os.path.exists(filename):
-        #os.remove(filename)
-        urlCant = 'https://d3e6htiiul5ek9.cloudfront.net/prod/productos?id_sucursal=' + sucursal + '&limit=100&offset=' + str(
-                    offset)
-        rTot = requests.get(urlCant, headers=headers).json()
-
-        if isinstance(rTot['total'], int):
-
-            totalElementos = rTot['total']
-
-            while offset <= totalElementos:
-                url = 'https://d3e6htiiul5ek9.cloudfront.net/prod/productos?id_sucursal=' + sucursal + '&limit=100&offset=' + str(
-                    offset)
-                r2 = requests.get(url, headers=headers).json()
-
-                for element in r2['productos']:
-                    tipodatoList[elementPos] = element
-                    elementPos = elementPos + 1
-
-                offset = offset + 99
-
-        with open(filename, 'w') as outfile:
-            json.dump(tipodatoList, outfile)
-
+importarProductos()
--- testPreciosProd/procPedido.py
+++ testPreciosProd/procPedido.py
@@ -1,28 +1,48 @@
-import pandas
 import os
 import json
-from decimal import Decimal
+from math import sin, cos, sqrt, atan2, radians
 
-listaProductosId = ['7798062547788', '7500435012409', '7790895000997']
-sucursales = []
 
-#for element in listaProductosId:
-    #print(element)
+def getSucursalesCerca(custlat, custlon, custdist):
 
-listaProdsEnSuc = []
+    # approximate radius of earth in km
+    R = 6373.0
+    sucsresult = []
+    custlat = radians(float(custlat))
+    custlon = radians(float(custlon))
 
-path = 'productos'
+    with open('sucursales/sucursales.json', 'r') as fileSucursales:
+        jsonsucursales = json.load(fileSucursales)
 
-#filtrar sucursales cerca
+        for elemsucursal in jsonsucursales:
+            suclat = radians(float(jsonsucursales[elemsucursal]['lat']))
+            suclon = radians(float(jsonsucursales[elemsucursal]['lng']))
+            dlon = suclon - custlon
+            dlat = suclat - custlat
+            #print(dlat, '-', dlon)
+            a = sin(dlat / 2)**2 + cos(custlat) * cos(suclat) * sin(dlon / 2)**2
+            c = 2 * atan2(sqrt(a), sqrt(1 - a))
+            distance = R * c
 
-def getProds(listaProductosId):
+
+            if distance <= custdist:
+                #print("Result:", jsonsucursales[elemsucursal]['id'], '-', distance)
+                sucsresult.append([jsonsucursales[elemsucursal]['id'], distance])
+
+    return sucsresult
+
+def getSucsConProds(listaProductosId, sucscandidatas):
+
+    path = 'productos'
+
     if os.path.exists(path):
+        listaSucsConProd = []
 
-        for sucursal in os.listdir(path):
+        for sucursal in sucscandidatas:
+            print(path+'/'+sucursal[0]+'.json')
 
-            if sucursal.endswith('.json'):
-
-                with open(path+'/'+sucursal, 'r') as listado:
+            try:
+                with open(path+'/'+sucursal[0]+'.json', 'r') as listado:
                     listadoJson = json.load(listado)
                     listaTemp=[]
                     for element in listadoJson:
@@ -30,37 +50,52 @@ def getProds(listaProductosId):
                         if listadoJson[element]['id'] in listaProductosId:
                             listaTemp.append(
                                 [
-                                    os.path.splitext(sucursal)[0],
+                                    sucursal[0],
                                     listadoJson[element]['nombre'],
                                     listadoJson[element]['marca'],
                                     listadoJson[element]['precio']
                                 ]
                             )
-                    if len(listaTemp) == 3:
-                        listaProdsEnSuc.append(os.path.splitext(sucursal)[0])
+                    if len(listaTemp) >= len(listaProductosId):
+                        listaSucsConProd.append(sucursal)
+            except:
+                print('No existe el archivo')
 
-    #print(listaProdsEnSuc)
-    return listaProdsEnSuc
+    #print(listaSucsConProd)
+    return listaSucsConProd
 
 
-def getMejorPrecio(sucursales, listaProductosId):
+def getPrecios(sucursales, listaProductosId):
     dictPrecios = {}
+    path = 'productos'
 
     for sucursal in sucursales:
-        dictPrecios[sucursal] = [0]
+        idSucursal = sucursal[0]
+        dictPrecios[idSucursal] = {}
+        dictPrecios[idSucursal]['suma'] = 0
+        dictPrecios[idSucursal]['distancia'] = sucursal[1]
+        dictPrecios[idSucursal]['productos'] = {}
+
         listaSucursal = []
 
-        with open(path + '/' + sucursal + '.json', 'r') as listado:
+        with open(path + '/' + idSucursal + '.json', 'r') as listado:
             listadoJson = json.load(listado)
 
             for element in listadoJson:
                 # print(listadoJson[element]['id'])
-
                 if listadoJson[element]['id'] in listaProductosId:
-                    dictPrecios[sucursal][0] = round(dictPrecios[sucursal][0] + float(listadoJson[element]['precio']), 2)
-                    dictPrecios[sucursal].append([listadoJson[element]['id'], listadoJson[element]['precio']])
+                    dictPrecios[idSucursal]['suma'] = round(dictPrecios[idSucursal]['suma'] + float(listadoJson[element]['precio']), 2)
+                    dictPrecios[idSucursal]['productos'][listadoJson[element]['id']] = listadoJson[element]['precio']
     return dictPrecios
 
 
-sucursales = getProds(listaProductosId)
-print(getMejorPrecio(sucursales, listaProductosId))
+def devolverResultado(lat, lon, listaProductosId):
+
+    sucscandidatas = getSucursalesCerca(lat, lon, 1)
+    #print(sucscandidatas)
+    listaSucsConProd = getSucsConProds(listaProductosId, sucscandidatas)
+    print(getPrecios(listaSucsConProd, listaProductosId))
+
+
+#devolverResultado('-34.568485', '-58.435059', ['7798062547788', '7500435012409', '7790895000997'])
+devolverResultado('-34.5633', '-58.5585', ['7798062547788', '7500435012409', '7790895000997'])
